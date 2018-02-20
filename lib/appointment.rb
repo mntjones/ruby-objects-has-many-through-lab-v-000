17a767f5fa9c032d58ccad082e4ae40efc3c5228
Monica Tamaru Jones <mntamaru@gmail.com>
2018-02-20 16:18:29 +0000

    automatically backed up by learn

---
+++ lib/appointment.rb
@@ -0,0 +1,18 @@
+class Appointment
+  
+  attr_accessor :date, :doctor, :patient
+  
+  def initialize(date, doctor)
+    @date = date
+    @doctor = doctor
+  end
+  
+  def patient
+   
+  end
+  
+  def doctor
+    
+  end
+  
+end
